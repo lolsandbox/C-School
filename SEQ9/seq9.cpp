90066a9f011293afd26573d05b47c5df35fadef8
AlongNgu <thanhbesthresh@gmail.com>
2023-03-13 00:14:17 +0700

    seq16 full test seq9 full test

--- SEQ9/seq9.cpp
+++ SEQ9/seq9.cpp
@@ -1,56 +1,94 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
 
+// using namespace std;
+
+// int n, k, s[1000005], pos[1000005], x;
+
+// void qs(int l, int r) {
+//     if (l >= r) return;
+
+//     int i = l, j = r, p = s[(l+r)/2];
+//     while (i < j) {
+//         while (s[i] < p) ++i;
+//         while (s[j] > p) --j;
+
+//         if (i > j) break;
+
+//         swap(s[i], s[j]);
+//         swap(pos[i], pos[j]);
+
+//         ++i; --j;
+//     }
+
+//     qs(l, j); qs(i, r);
+// }
+
+// int main() {
+//     freopen("seq9.inp", "r", stdin);
+//     freopen("seq9.out", "w", stdout);
+
+//     cin >> n >> k;
+//     s[0] = 0;
+//     for (int i = 1; i <= n; ++i) {
+//         cin >> x;
+//         s[i] = s[i-1] + x;
+//         pos[i] = i;
+//         s[i] = s[i] % k;
+//     }
+
+//     qs(1, n);
+
+// //    for (int i = 1; i <= n; ++i) cout << s[i] << " ";
+// //    cout << "\n";
+// //    for (int i = 1; i <= n; ++i) cout << pos[i] << " ";
+
+//     int l = 1;
+//     int kq = 0;
+//     for (int r = 1; r <= n; ++r) {
+//         if (s[l] != s[r]) l = r;
+//         kq =  max(kq, pos[r]-pos[l]);
+//         if (s[r] == 0) kq = max(kq, pos[r]);
+//     }
+
+//     cout << kq << "\n";
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, k, s[1000005], pos[1000005], x;
-
-void qs(int l, int r) {
-    if (l >= r) return;
-
-    int i = l, j = r, p = s[(l+r)/2];
-    while (i < j) {
-        while (s[i] < p) ++i;
-        while (s[j] > p) --j;
-
-        if (i > j) break;
-
-        swap(s[i], s[j]);
-        swap(pos[i], pos[j]);
-
-        ++i; --j;
+int find_longest_subarray_divisible_by_k(vector<int> a, int n, int k) {
+    vector<int> count_mod(k);
+    int sum = 0, longest_len = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+        sum %= k;
+        if (sum == 0) {
+            longest_len = i+1;
+        } else if (count_mod[sum] > 0) {
+            longest_len = max(longest_len, i-count_mod[sum]+1);
+        } else {
+            count_mod[sum] = i+1;
+        }
     }
-
-    qs(l, j); qs(i, r);
+    return longest_len;
 }
 
 int main() {
+    int n, k;
+
     freopen("seq9.inp", "r", stdin);
     freopen("seq9.out", "w", stdout);
 
     cin >> n >> k;
-    s[0] = 0;
-    for (int i = 1; i <= n; ++i) {
-        cin >> x;
-        s[i] = s[i-1] + x;
-        pos[i] = i;
-        s[i] = s[i] % k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
-
-    qs(1, n);
-
-//    for (int i = 1; i <= n; ++i) cout << s[i] << " ";
-//    cout << "\n";
-//    for (int i = 1; i <= n; ++i) cout << pos[i] << " ";
-
-    int l = 1;
-    int kq = 0;
-    for (int r = 1; r <= n; ++r) {
-        if (s[l] != s[r]) l = r;
-        kq =  max(kq, pos[r]-pos[l]);
-        if (s[r] == 0) kq = max(kq, pos[r]);
-    }
-
-    cout << kq << "\n";
-
+    
+    int longest_len = find_longest_subarray_divisible_by_k(a, n, k);
+    cout << longest_len << endl;
+    
     return 0;
 }
---
+++ seq16/seq16.cpp
@@ -0,0 +1,80 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// const int maxN = 1e6 + 5;
+
+// int n;
+// float k;
+// vector<int> args(maxN);
+
+// int main() {
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+
+//     freopen("aver.inp", "r", stdin);
+//     freopen("aver.out", "w", stdout);
+
+//     cin >> n >> k;
+    
+//     args.resize(n);
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> args[i];
+//     }
+
+//     int ans = 0;
+
+//     for (int i = 0; i < n; i++) {
+//         float sum = 0;
+//         for (int j = i; j < n; j++) {
+//             sum += args[j];
+//             float avg = sum / (j - i + 1);
+//             // cout << avg << endl;
+//             if (avg >= k) {
+//                 ans = max(ans, j - i + 1);
+//             }
+//         }
+//     }
+
+//     cout << ans << endl;
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+pair <long long, int> f[1000007];
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("seq16.inp", "r", stdin);
+    freopen("seq16.out", "w", stdout);
+    cin >> n >> k;
+    for (int i = 1; i <= n; ++i) {
+        int a;
+        cin >> a;
+        f[i].first = f[i - 1].first + (a - k);
+        f[i].second = i;
+    }
+
+    sort(f + 1, f + n + 1);
+
+    int vtmn = INT_MAX, ans = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        vtmn = min(vtmn, f[i].second);
+        if (f[i].second > vtmn) {
+            ans = max(ans, f[i].second - (vtmn + 1) + 1);
+        }
+        if (f[i].first >= 0) {
+            ans = max(ans, f[i].second);
+        }
+    }
+    cout << ans << "\n";
+
+    return 0;
+}
