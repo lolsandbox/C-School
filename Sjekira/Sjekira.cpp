d04e77c6db741a86b9be44452c078edb8ae5d2e4
AlongNgu <thanhbesthresh@gmail.com>
2023-03-29 22:21:20 +0700

    a

---
+++ Sjekira/Sjekira.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    freopen("Sjekira.inp", "r", stdin);
+    freopen("Sjekira.out", "w", stdout);
+
+    cin >> 
+    return 0;
+}
